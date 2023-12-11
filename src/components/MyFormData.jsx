57266ca19bde78ffdd0cf5f4448464a5980e4dd8
sim-sal <simone.saladino93@gmail.com>
2023-12-11 04:25:55 +0100

    made a new api call to obtain the list of tags from the db and hooked the value of the chackboxes

--- src/components/MyFormData.jsx
+++ src/components/MyFormData.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import style from "../css/modules/MyFormData.module.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const initialFormData = {
     title: "",
@@ -17,14 +17,8 @@ export default function MyFormData() {
     const [postsList, setPostsList] = useState([]);
     const [formData, setFormData] = useState(initialFormData);
     const [formVisible, setFormVisible] = useState(false); // Stato per gestire la visibilità del form
+    const [tagsList, setTagsList] = useState([]);
 
-
-    function updateFormData(newValue, fieldName) {
-        setFormData({
-            ...formData,
-            [fieldName]: newValue
-        });
-    }
 
     function handleFormSubmit(e) {
         e.preventDefault();
@@ -34,25 +28,49 @@ export default function MyFormData() {
         setFormData(initialFormData);
     }
 
-    function removePost(idToRemove) {
-        setPostsList(postsList.filter((post) => post.id !== idToRemove));
-    }
-
     function handleInputChange(e, key) {
-        // recupero i value
-        const value = e.target.value;
+        const { value, checked, type } = e.target;
 
-        // recupero lo stato della checkbox
-        const checked = e.target.checked;
+        if (key === "tags") {
+            // Se la chiave è "tags", gestisci l'array di tag
+            setFormData(prev => {
+                let currentTags = [...prev.tags]; // Crea una copia dell'array esistente
 
-        // prendo i valori attuali + quelli nuovi
-        setFormData(prev => {
-            return {
-                ...prev,
-                [key]: value
-            }
-        })
+                if (checked) {
+                    // Aggiungi il tag se è stato selezionato
+                    currentTags.push(value);
+                } else {
+                    // Rimuovi il tag se è stato deselezionato
+                    currentTags = currentTags.filter(tag => tag !== value);
+                }
+
+                // Restituisci il nuovo stato
+                return {
+                    ...prev,
+                    [key]: currentTags
+                };
+            });
+        } else {
+            // Se la chiave non è "tags", gestisci normalmente
+            setFormData(prev => {
+                return {
+                    ...prev,
+                    [key]: type === "checkbox" ? checked : value
+                };
+            });
+        }
     }
+
+    async function fetchTags() {
+        const tags = await (await fetch("http://localhost:3000/tags")).json();
+
+        setTagsList(tags);
+    }
+
+    // invoco la funzione fetch alla "creazione" del componente
+    useEffect(() => {
+        fetchTags();
+    }, []);
 
     return (
 
@@ -75,70 +93,88 @@ export default function MyFormData() {
                     <form onSubmit={handleFormSubmit}>
 
                         <div className="mb-3 mt-3">
-                            <label htmlFor="titlePost" className="form-label">
-                                <FontAwesomeIcon icon="fa-solid fa-tornado" />
-                                <strong> Titolo:</strong>
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Inserisci qui il titolo del post"
-                                name="title_input"
-                                value={formData.title}
-                                onChange={(e) => handleInputChange(e, "title")}
-                            ></input>
+                            <div>
+                                <label htmlFor="titlePost" className="form-label">
+                                    <FontAwesomeIcon icon="fa-solid fa-tornado" />
+                                    <strong> Titolo:</strong>
+                                </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Inserisci qui il titolo del post"
+                                    name="title_input"
+                                    value={formData.title}
+                                    onChange={(e) => handleInputChange(e, "title")}
+                                ></input>
+                            </div>
 
-                            <label htmlFor="imgPost" className="form-label">
-                                <FontAwesomeIcon icon="fa-solid fa-image" />
-                                <strong> URL Immagine:</strong>
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Inserisci l'URL dell'immagine"
-                                name="img_input"
-                                value={formData.image}
-                                onChange={(e) => handleInputChange(e, "image")}
-                            ></input>
+                            <div>
+                                <label htmlFor="imgPost" className="form-label">
+                                    <FontAwesomeIcon icon="fa-solid fa-image" />
+                                    <strong> URL Immagine:</strong>
+                                </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Inserisci l'URL dell'immagine"
+                                    name="img_input"
+                                    value={formData.image}
+                                    onChange={(e) => handleInputChange(e, "image")}
+                                ></input>
+                            </div>
 
-                            <label htmlFor="contentPost" className="form-label">
-                                <FontAwesomeIcon icon="fa-solid fa-box-open" />
-                                <strong> Contenuto:</strong>
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Inserisci qui il contenuto del post"
-                                name="content_input"
-                                value={formData.content}
-                                onChange={(e) => handleInputChange(e, "content")}
-                            ></input>
+                            <div>
+                                <label htmlFor="contentPost" className="form-label">
+                                    <FontAwesomeIcon icon="fa-solid fa-box-open" />
+                                    <strong> Contenuto:</strong>
+                                </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Inserisci qui il contenuto del post"
+                                    name="content_input"
+                                    value={formData.content}
+                                    onChange={(e) => handleInputChange(e, "content")}
+                                ></input>
+                            </div>
 
-                            <label htmlFor="categoryPost" className="form-label">
-                                <FontAwesomeIcon icon="fa-solid fa-certificate" />
-                                <strong> Categoria:</strong>
-                            </label><br />
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Inserisci la categoria"
-                                name="category_input"
-                                value={formData.category}
-                                onChange={(e) => handleInputChange(e, "category")}
-                            ></input>
+                            <div>
+                                <label htmlFor="categoryPost" className="form-label">
+                                    <FontAwesomeIcon icon="fa-solid fa-certificate" />
+                                    <strong> Categoria:</strong>
+                                </label><br />
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Inserisci la categoria"
+                                    name="category_input"
+                                    value={formData.category}
+                                    onChange={(e) => handleInputChange(e, "category")}
+                                ></input>
+                            </div>
 
-                            <label htmlFor="tagsPost" className="form-label">
-                                <FontAwesomeIcon icon="fa-solid fa-hashtag" />
-                                <strong> Tags:</strong>
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Inserisci i tags"
-                                name="tags_input"
-                                value={formData.tags}
-                                onChange={(e) => handleInputChange(e, "tags")}
-                            ></input>
+                            <div>
+                                <label htmlFor="tagsPost" className="form-label">
+                                    <FontAwesomeIcon icon="fa-solid fa-hashtag" />
+                                    <strong> Tags:</strong>
+                                </label>
+                                <div className="row">
+                                    {tagsList.map(tag => {
+                                        return <div className="col-3">
+                                            <label key={tag.id}>
+                                                {tag.name}
+
+                                                <input
+                                                    type="checkbox"
+                                                    name="tags_input"
+                                                    value={tag.id}
+                                                    onChange={(e) => handleInputChange(e, "tags")}
+                                                ></input>
+                                            </label>
+                                        </div>
+                                    })}
+                                </div>
+                            </div>
 
                             <div className="mt-4">
                                 <label htmlFor="publishedPost"><FontAwesomeIcon icon="fa-solid fa-clipboard-check" /><strong> Pubblica</strong></label>
